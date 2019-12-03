c7369be2564b3cadf9324060d6d4a7715513b0db
Evan Roubekas <35752039+eroub@users.noreply.github.com>
2019-12-02 17:41:16 -0800

    reverted test script

--- test/test-suite.sh
+++ test/test-suite.sh
@@ -5,7 +5,7 @@ echo "***********************"
 echo "Running through unit tests via mocha ... "
 # istanbul cover --reporter=html --reporter=text mocha --require source-map-support/register --require ts-node/register --exit **/**/*.spec.ts
 # istanbul cover mocha --exit
-mocha -r ts-node/register --exit **/**/customermodel.spec.ts
+mocha -r ts-node/register --exit **/**/*.spec.ts
 echo "Running through static analysis via TSLint ... "
 tslint -c tslint/tslint.json '../src/**/*.ts'
 echo "Success ! "
