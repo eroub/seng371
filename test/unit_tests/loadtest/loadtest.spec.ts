be320fd6251e464da791abf5df3be6e88cbb8cb8
Asim <maali@uvic.ca>
2019-12-04 11:17:57 -0800

    loadtest

--- test/unit_tests/loadtest/loadtest.spec.ts
+++ test/unit_tests/loadtest/loadtest.spec.ts
@@ -1,31 +1,37 @@
 var loadtest = require('loadtest');
 var should = require('should');
+describe("Performance Test", function() {
+    let noRequestPerHour = 100000;
+    let avgRequestTime = 1000;
 
-//The system should not time out or make users wait over 0.75 seconds for the system to respond to their request.
-describe("Reliability under heavy user load", function() {
- let avgRequestTime:any = 7500;
-
-    it("performance testing", function(done) {
+    it("should not crash with 1000 users making requests", function(done) {
+        this.timeout(1000 * 60);
 
         const options = {
-            url: 'http://localhost:7000/user/1/allShoes',
-            maxSeconds : 30,
-            requestsPerSecond:100,
-            concurrency:50,
-            maxRequests: 1000,
+            url: 'http://localhost:7000',
+            maxSeconds: 5,
+            requestsPerSecond: 25,
+            concurrency: 1000,
+            statusCallback: statusCallback
         };
-        loadtest.loadTest(options, function(error:any, result:any)
-        {
-            if (error)
-            {
-                return consogle.error('Got an error: %s', error);
+
+        let gLatency:any;
+        function statusCallback(error: any, result: any, latency: any) {
+            console.log('Current latency %j, result %j, error %j', latency, result, error);
+        }
+
+        let operation = loadtest.loadTest(options, function(error:any,result:any) {
+            if (error) {
+                console.error('Got an error: %s', error)
+            } else if (operation.running == false) {
+
+                console.info("Requests/Second :", result.rps);
+
+                result.totalErrors.should.equal(0);
+                result.meanLatencyMs.should.be.lessThan(avgRequestTime);
+
+                done();
             }
-            console.log('Load test run successfully');
-            result.totalErrors.should.equal(0);
-            result.meanLatencyMs.should.be.below(avgRequestTime);
-            done();
-
         });
-
     });
 });
